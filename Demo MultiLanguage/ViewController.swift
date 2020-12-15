bfca8adabc1f1cc11be0eeb43182c6cb366fab2c
NguyenTienHoa <hoant@gemvietnam.com>
2020-12-15 15:13:09 +0700

    Initial Commit

---
+++ Demo MultiLanguage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Demo MultiLanguage
+//
+//  Created by GEM on 12/15/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
